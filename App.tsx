d7706e254a8ddb77403b4b3cd47f2413f4e7f04c
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-21 18:45:17 -0300

    first

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
 import { View, Text } from 'react-native';
 
 export const App = () => {
   return (
     <View>
-      <Text>Hola mundo</Text>
+      <Text>Hola React native</Text>
     </View>
   );
 };
