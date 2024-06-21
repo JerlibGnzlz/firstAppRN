1a4e5e4e582c2a05a20164cc52a10945821315b4
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-21 20:14:21 -0300

    buid componente hi

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { HelloWord } from './src/presentation/screens/HelloWord';
 
 
 export const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.contenedor}>
       <HelloWord />
     </SafeAreaView>
   );
 };
 
+
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    backgroundColor: "yellow"
+  }
+})
--- src/presentation/screens/HelloWord.tsx
+++ src/presentation/screens/HelloWord.tsx
@@ -1,10 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export const HelloWord = () => {
     return (
-        <View>
-            <Text>HelloWord</Text>
+        <View style={styles.contenedor}>
+            <Text style={styles.titulo}>HelloWord</Text>
         </View>
     )
 };
 
+
+
+const styles = StyleSheet.create({
+    contenedor: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    titulo: {
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold"
+    }
+
+})
