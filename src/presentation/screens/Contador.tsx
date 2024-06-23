5c6e52a1e4c6c10e6f96577f8ff2ea277c6c1d82
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-22 21:59:44 -0300

    style a los botones

--- src/presentation/screens/Contador.tsx
+++ src/presentation/screens/Contador.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Platform } from 'react-native';
 
 
 
@@ -10,20 +10,29 @@ export const Contador = () => {
 
     return (
         <View style={styles.contenedor}>
-            <Text style={styles.titulo}>{count}</Text>
+            <Text style={styles.titulo1}>{count}</Text>
 
             <Pressable
                 onPress={() => setCount(count + 1)}
                 onLongPress={() => { setCount(0) }}
+                style={({ pressed }) => [
+                    styles.boton,
+                    pressed && styles.botonPressed
+                ]}
             >
-                <Text style={styles.boton}>+1</Text>
+                <Text style={styles.titulo}>Incrementar</Text>
             </Pressable>
 
             <Pressable
                 onPress={() => setCount(count - 1)}
+
                 onLongPress={() => { setCount(0) }}
+                style={({ pressed }) => [
+                    styles.boton,
+                    pressed && styles.botonPressed
+                ]}
             >
-                <Text style={styles.boton}>-1</Text>
+                <Text style={styles.titulo}>Descrementar</Text>
             </Pressable>
         </View >
     )
@@ -38,10 +47,26 @@ const styles = StyleSheet.create({
     },
     titulo: {
         textAlign: 'center',
-        fontSize: 100,
+        fontSize: 30,
         fontWeight: 'bold',
     },
+    titulo1: {
+        textAlign: 'center',
+        fontSize: 100,
+        fontWeight: '900',
+        color: "black"
+    },
     boton: {
+        backgroundColor: Platform.OS === "android" ? "#5856D6" : "",
         fontSize: 40,
+        margin: 5,
+        borderRadius: 5,
+        padding: 5,
+        width: 300,
+        textAlign: 'center',
+        fontWeight: '800'
+    },
+    botonPressed: {
+        backgroundColor: "#323181",
     }
 });
