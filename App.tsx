719a98ed9229df77e4c8dc8e9ce5ef78ea78c92d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-22 23:06:31 -0300

    boton con paper

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { HelloWord } from './src/presentation/screens/HelloWord';
 import { Contador } from './src/presentation/screens/Contador';
+import { PaperProvider } from 'react-native-paper';
 
 
 export const App = () => {
   return (
-    <SafeAreaView style={styles.contenedor}>
-      {/* <HelloWord name={'Jerlib Jose Gonzalez Vega'} /> */}
-      <Contador />
-    </SafeAreaView>
+    <PaperProvider>
+
+      <SafeAreaView style={styles.contenedor}>
+        {/* <HelloWord name={'Jerlib Jose Gonzalez Vega'} /> */}
+        <Contador />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 
