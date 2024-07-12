453c5a90b6faf36945c5c206f2d8c5b6ff8be8ee
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-12 18:01:25 -0300

    flex direection tarea

--- src/presentation/screens/FlexBox.tsx
+++ src/presentation/screens/FlexBox.tsx
@@ -9,17 +9,6 @@ export const FlexBox = () => {
             <View style={[styles.box, styles.box1]} />
             <View style={[styles.box, styles.box2]} />
             <View style={[styles.box, styles.box3]} />
-            <View style={[styles.box, styles.box1]} />
-            <View style={[styles.box, styles.box2]} />
-            <View style={[styles.box, styles.box3]} />
-            <View style={[styles.box, styles.box1]} />
-            <View style={[styles.box, styles.box2]} />
-            <View style={[styles.box, styles.box3]} />
-            <View style={[styles.box, styles.box1]} />
-            <View style={[styles.box, styles.box2]} />
-            <View style={[styles.box, styles.box3]} />
-
-
         </View >
     )
 };
@@ -28,30 +17,46 @@ const styles = StyleSheet.create({
     contenedor: {
         flex: 1,
         backgroundColor: "gray",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "stretch",
-        flexWrap: "wrap",
-        gap: 10
+        flexDirection: "column",
+        justifyContent: "center",
+        // alignItems: "center",
+        // flexWrap: "wrap",
+        // gap: 40
     },
 
     box: {
         // flex: 1,
         height: 100,
-        width: 100,
+        // width: 100,
+        borderColor: "white",
+        borderWidth: 4
     },
     box1: {
+        // flex: 1,
         backgroundColor: "yellow",
+        width: 100,
+
+        // alignSelf: "center"
 
     },
 
     box2: {
+        // flex: 1,
         backgroundColor: "blue",
+        width: 100,
+
+        // alignSelf: "center",
+        // left: 100,
+        // bottom: -400
+
 
     },
     box3: {
-        // alignSelf: "center",
+        // flex: 1,
         backgroundColor: "red",
+        alignSelf: "stretch",
+        // width: 100
+        // top: 150
 
 
     },
